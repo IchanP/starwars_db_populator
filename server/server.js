f2f09151d0d1ea9cd453b8865552fa95294d06c6
Pontus <smeaurix@gmail.com>
2025-03-18 10:26:08 +0100

    prisma dependency

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config';
 import mercurius from 'mercurius';
 import { typeDefs } from './graphql/schema/mergeTypeDefs.js';
 import { makeExecutableSchema } from '@graphql-tools/schema';
+import { context } from './context.js';
 
 const fastify = Fastify({
   logger: true
@@ -23,7 +24,8 @@ const schema = makeExecutableSchema({
 fastify.register(mercurius, {
   schema,
   // TODO: Add resolvers here.
-  graphiql: true
+  graphiql: true,
+  context
 })
 
 fastify.listen({ port: 4000 }, function (err) {
@@ -33,3 +35,19 @@ fastify.listen({ port: 4000 }, function (err) {
     process.exit(1)
   }
 })
+
+
+const shutdown = async () => {
+  console.log('Shutting down gracefully...');
+  const prisma = context()
+  await prisma.$disconnect(); // Disconnect Prisma
+  await fastify.close(); // Close Fastify server
+  console.log('Server has been gracefully shut down.');
+  // eslint-disable-next-line no-undef
+  process.exit(0);
+};
+
+// eslint-disable-next-line no-undef
+process.on('SIGINT', shutdown);
+// eslint-disable-next-line no-undef
+process.on('SIGTERM', shutdown);
