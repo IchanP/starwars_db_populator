397da32d2e098f7c681214f45fe2f354beb077e0
Pontus <smeaurix@gmail.com>
2025-03-18 14:58:48 +0100

    add boiler plate

---
+++ ts-server/src/graphql/resolvers/index.ts
@@ -0,0 +1,8 @@
+// Helper methods for resolving data
+
+import { Context } from "../../context";
+
+// This function is dumb
+export const convertToContextType = (context: any): Context => {
+  return context;
+};
--- ts-server/src/graphql/resolvers/resolver.ts
+++ ts-server/src/graphql/resolvers/resolver.ts
@@ -1,18 +1,18 @@
 import { IResolvers, MercuriusContext } from "mercurius";
 import { Context } from "../../context";
+import { convertToContextType } from ".";
 
 export const resolvers = {
   Query: {
     films: async (_parent: any, args: any, cxt: any) => {
-      const content = cxt as Context;
-      // TODO prisma is undefined...
-      console.log(content.prisma);
-      const films = await content.prisma.starwars_film.findMany();
-      console.log(films);
-      return films.map((film) => ({
-        ...film,
-        id: film.id.toString(), // Convert BigInt to string
-      }));
+      const context = convertToContextType(cxt);
+      return await context.prisma.starwars_film.findMany();
+    },
+    film: async (_parent: any, args: any, cxt: any) => {
+      const context = convertToContextType(cxt);
+      return await context.prisma.starwars_film.findUnique({
+        where: { id: Number(args.id) },
+      });
     },
   },
 };
