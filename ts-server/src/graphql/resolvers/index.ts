897bd60496807af847b480fca311b103f459c5bb
Pontus <smeaurix@gmail.com>
2025-03-19 07:41:29 +0100

    add top level queries

--- ts-server/src/graphql/resolvers/index.ts
+++ ts-server/src/graphql/resolvers/index.ts
@@ -2,7 +2,11 @@
 
 import { Context } from "../../context";
 
-// This function is dumb
-export const convertToContextType = (context: any): Context => {
-  return context;
+// Helper function to handle common logic
+export const withContext = async (
+  cxt: any,
+  callback: (context: Context) => any
+) => {
+  const context = cxt as Context;
+  return await callback(context);
 };
--- ts-server/src/graphql/resolvers/resolver.ts
+++ ts-server/src/graphql/resolvers/resolver.ts
@@ -1,18 +1,103 @@
 import { IResolvers, MercuriusContext } from "mercurius";
+import { withContext } from ".";
 import { Context } from "../../context";
-import { convertToContextType } from ".";
 
-export const resolvers = {
+export const resolvers: IResolvers = {
   Query: {
-    films: async (_parent: any, args: any, cxt: any) => {
-      const context = convertToContextType(cxt);
-      return await context.prisma.starwars_film.findMany();
-    },
-    film: async (_parent: any, args: any, cxt: any) => {
-      const context = convertToContextType(cxt);
-      return await context.prisma.starwars_film.findUnique({
-        where: { id: Number(args.id) },
-      });
-    },
+    films: (_parent: any, args: any, cxt: any) =>
+      withContext(cxt, (context) => context.prisma.starwars_film.findMany()),
+
+    film: (_parent: any, args: any, cxt: any) =>
+      withContext(cxt, (context) =>
+        context.prisma.starwars_film.findUnique({
+          where: { id: Number(args.id) },
+        })
+      ),
+
+    people: (_parent: any, args: any, cxt: any) =>
+      withContext(cxt, (context) => context.prisma.starwars_people.findMany()),
+
+    person: (_parent: any, args: any, cxt: any) =>
+      withContext(cxt, (context) =>
+        context.prisma.starwars_people.findUnique({
+          where: { id: Number(args.id) },
+        })
+      ),
+
+    planets: (_parent: any, args: any, cxt: any) =>
+      withContext(cxt, (context) => context.prisma.starwars_planet.findMany()),
+
+    planet: (_parent: any, args: any, cxt: any) =>
+      withContext(cxt, (context) =>
+        context.prisma.starwars_planet.findUnique({
+          where: { id: Number(args.id) },
+        })
+      ),
+
+    species: (_parent: any, args: any, cxt: any) =>
+      withContext(cxt, (context) => context.prisma.starwars_species.findMany()),
+
+    speciesById: (_parent: any, args: any, cxt: any) =>
+      withContext(cxt, (context) =>
+        context.prisma.starwars_species.findUnique({
+          where: { id: Number(args.id) },
+        })
+      ),
+
+    starships: (_parent: any, args: any, cxt: any) =>
+      withContext(cxt, (context) =>
+        context.prisma.starwars_starship.findMany({
+          include: {
+            starwars_transport: true,
+          },
+        })
+      ),
+
+    starship: (_parent: any, args: any, cxt: any) =>
+      withContext(cxt, (context) =>
+        context.prisma.starwars_starship.findUnique({
+          where: {
+            transport_ptr_id: Number(args.id),
+          },
+          include: {
+            starwars_transport: true,
+          },
+        })
+      ),
+
+    transports: (_parent: any, args: any, cxt: any) =>
+      withContext(cxt, (context) =>
+        context.prisma.starwars_transport.findMany()
+      ),
+
+    transport: (_parent: any, args: any, cxt: any) =>
+      withContext(cxt, (context) =>
+        context.prisma.starwars_transport.findUnique({
+          where: {
+            id: Number(args.id),
+          },
+        })
+      ),
+
+    vehicles: (_parent: any, args: any, cxt: any) =>
+      withContext(cxt, (context) =>
+        context.prisma.starwars_vehicle.findMany({
+          include: {
+            starwars_transport: true,
+          },
+        })
+      ),
+
+    vehicle: (_parent: any, args: any, cxt: any) =>
+      withContext(cxt, (context) =>
+        context.prisma.starwars_vehicle.findUnique({
+          where: {
+            transport_ptr_id: Number(args.id),
+          },
+          include: {
+            starwars_transport: true,
+          },
+        })
+      ),
   },
 };
