0edcdcc2f9a1e59df1dc47ebe8a6f5df210e87c3
Pontus <smeaurix@gmail.com>
2025-03-28 11:13:16 +0100

    add people, species starship cache

--- ts-server/src/graphql/resolvers/ex2/cache/peopleResolver.ts
+++ ts-server/src/graphql/resolvers/ex2/cache/peopleResolver.ts
@@ -1,27 +1,47 @@
 import { MercuriusContext } from "mercurius";
 import { withContext } from "../../";
 import { Character } from "../../Person";
+import { getFromCache, setCache } from ".";
 
 export const Person = {
   homeworld: (person: Character, _args: unknown, cxt: MercuriusContext) =>
-    withContext(cxt, async (context) =>
-      context.prisma.starwars_planet.findUnique({
-        where: { id: person.homeworld_id },
-      })
-    ),
+    withContext(cxt, async (context) => {
+      const cacheKey = `personHomeworld:${person.id}`;
+      let homeworld = await getFromCache(cacheKey, context.redis);
+
+      if (!homeworld) {
+        homeworld = await context.prisma.starwars_planet.findUnique({
+          where: { id: person.homeworld_id },
+        });
+
+        await setCache(cacheKey, homeworld, context.redis);
+      }
+
+      return homeworld;
+    }),
 
   species: (person: Character, _args: unknown, cxt: MercuriusContext) =>
     withContext(cxt, async (context) => {
-      const speciesPeople =
-        await context.prisma.starwars_species_people.findMany({
-          where: { people_id: person.id },
-          include: {
-            starwars_species: true,
-          },
-        });
-      return speciesPeople.map(
-        (species: SpeciesPerson) => species.starwars_species
-      );
+      const cacheKey = `personSpecies:${person.id}`;
+      let species = await getFromCache(cacheKey, context.redis);
+
+      if (!species) {
+        const speciesPeople =
+          await context.prisma.starwars_species_people.findMany({
+            where: { people_id: person.id },
+            include: {
+              starwars_species: true,
+            },
+          });
+
+        species = speciesPeople.map(
+          (species: SpeciesPerson) => species.starwars_species
+        );
+
+        await setCache(cacheKey, species, context.redis);
+      }
+
+      return species;
     }),
 };
 
--- ts-server/src/graphql/resolvers/ex2/cache/starshipResolver.ts
+++ ts-server/src/graphql/resolvers/ex2/cache/starshipResolver.ts
@@ -1,6 +1,7 @@
 import { MercuriusContext } from "mercurius";
 import { withContext } from "../../";
 import { StarshipInterface } from "../../Starship";
+import { getFromCache, setCache } from ".";
 
 export const Starship = {
   pilots: (
@@ -9,16 +10,26 @@ export const Starship = {
     cxt: MercuriusContext
   ) =>
     withContext(cxt, async (context) => {
-      const starshipPilots =
-        await context.prisma.starwars_starship_pilots.findMany({
-          where: { starship_id: starship.transport_ptr_id },
-          include: {
-            starwars_people: true,
-          },
-        });
-      return starshipPilots.map(
-        (people: StarshipPilot) => people.starwars_people
-      );
+      const cacheKey = `starshipPilots:${starship.transport_ptr_id}`;
+      let pilots = await getFromCache(cacheKey, context.redis);
+
+      if (!pilots) {
+        const starshipPilots =
+          await context.prisma.starwars_starship_pilots.findMany({
+            where: { starship_id: starship.transport_ptr_id },
+            include: {
+              starwars_people: true,
+            },
+          });
+
+        pilots = starshipPilots.map(
+          (people: StarshipPilot) => people.starwars_people
+        );
+
+        await setCache(cacheKey, pilots, context.redis);
+      }
+
+      return pilots;
     }),
 
   starwars_transport: (
@@ -26,11 +37,20 @@ export const Starship = {
     _args: unknown,
     cxt: MercuriusContext
   ) =>
-    withContext(cxt, async (context) =>
-      context.prisma.starwars_transport.findUnique({
-        where: { id: starship.transport_ptr_id },
-      })
-    ),
+    withContext(cxt, async (context) => {
+      const cacheKey = `starshipTransport:${starship.transport_ptr_id}`;
+      let transport = await getFromCache(cacheKey, context.redis);
+
+      if (!transport) {
+        transport = await context.prisma.starwars_transport.findUnique({
+          where: { id: starship.transport_ptr_id },
+        });
+
+        await setCache(cacheKey, transport, context.redis);
+      }
+
+      return transport;
+    }),
 };
 
 interface StarshipPilot {
