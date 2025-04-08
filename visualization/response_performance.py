c8a5a1600165d0c941d3dcf3f6106e24bba8ea53
Pontus <smeaurix@gmail.com>
2025-04-08 13:26:52 +0200

    change x label

--- visualization/response_performance.py
+++ visualization/response_performance.py
@@ -5,7 +5,7 @@ import glob
 
 machine = "data_machine_2"
 
-path = "./data/" + machine +"/response_times/ex1_*.csv"
+path = "./data/" + machine + "/response_times/ex1_*.csv"
 
 # Define the path to your data files
 data_files = glob.glob(path)
@@ -21,13 +21,15 @@ fig = make_subplots(rows=1, cols=num_plots, subplot_titles=[f"{(i+1) * 100} User
 for i, df in enumerate(data_frames):
     totals = df[df['Label'] == 'TOTAL']
 
+    totals['Label'] = 'Avg. Response Time'
+
     fig.add_trace(go.Bar(
         x=totals['Label'],
         y=totals['Average'],
         error_y=dict(
             type='data',
             array=totals['Std. Dev.'],
-            visible=True
+            visible=True,
         ),
         name='Average Response Time with Std. Dev.'
     ), row=1, col=i+1)
@@ -38,9 +40,5 @@ fig.update_layout(height=600, width=2000, title_text="Response Times Under Varyi
 y_max = 2000
 for i in range(1, num_plots + 1):
     fig.update_yaxes(ticksuffix=" ms", row=1, range=[0, y_max], col=i)
-
-
-for i in range(1, num_plots + 1):
-    fig.update_xaxes(title_text="Average Response Time", row=1, col=i)
 
 fig.show()
